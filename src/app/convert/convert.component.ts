07dd63dbeb490ee4c1b2ab8e25effc8d7ca65772
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-04-22 15:07:32 +0200

    styling Gjort konverteringssidan

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.scss'
 })
+
 export class ConvertComponent {
 
+  valueToConvert: number = 0;
+  convertedValue: number = 0;
+  conversionText: string = '';
+
+  convertToMeter(): void {
+    this.conversionText = ''; // Reset conversion text
+    let meter: number = this.valueToConvert; 
+    this.convertedValue = meter / 3.28084; 
+    this.conversionText = `Konverterade ${meter} fot till ${this.convertedValue.toFixed(3)} meter.`;
+  }
+  convertToFeet(): void {
+    this.conversionText = ''; // Reset conversion text
+    let feet: number = this.valueToConvert;
+    this.convertedValue = feet * 3.28084;
+    this.conversionText = `Konverterade ${feet} meter till ${this.convertedValue.toFixed(3)} fot.`;
+  }
+
+  convertToFahrenheit(): void {
+    this.conversionText = ''; // Reset conversion text
+    let celsius: number = this.valueToConvert;
+    this.convertedValue = (celsius * 9 / 5) + 32; 
+    this.conversionText = `Konverterade ${celsius} 🌡︎ Celsius till ${this.convertedValue.toFixed(3)} 🌡︎ Fahrenheit.`;
+  }
+
+  convertToCelsius(): void {
+    this.conversionText = ''; // Reset conversion text
+    let fahrenheit: number = this.valueToConvert;
+    this.convertedValue = (fahrenheit - 32) * 5 / 9;
+    this.conversionText = `Konverterade ${fahrenheit} 🌡︎ Fahrenheit till ${this.convertedValue.toFixed(3)} 🌡︎ Celsius.`;
+  }
 }
