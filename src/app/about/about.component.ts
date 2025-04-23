e0b7e80aa6213b15a7c0ccb8785dd27b77b99820
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-04-23 13:21:53 +0200

    Aboutsidan klar (hoppas jag) Bilder för den sidan styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  randomPicturePath: string;
 
+  constructor() {
+    this.randomPicturePath = this.randomUrl();
+  }
+
+  randomUrl(): string {
+    const pictures: string[] = ["images/Tobbe1.jpg", "images/Tobbe2.jpg", "images/Tobbe3.jpg", "images/Tobbe4.jpg"];
+    const randomIndex: number = Math.floor(Math.random() * pictures.length);
+    return pictures[randomIndex];
+  }
 }
+
+
